8ff66583cd14cdcc20985238c96fa89f78c7e23c
DoDoTeamSandA <dodsolutions4@gmail.com>
2021-05-20 11:39:15 +0530

    testing git 1

--- app/src/main/java/com/sunny/dodof/OrderIdWeight.java
+++
@@ -1,6 +0,0 @@
-package com.sunny.dodof;
-
-
-public interface OrderIdWeight {
-    public void onOrderIdWeight(String[] total);
-}
--- app/src/main/java/com/sunny/dodof/TotalListener.java
+++
@@ -1,6 +0,0 @@
-package com.sunny.dodof;
-
-
-public interface TotalListener {
-    public void onTotalChanged(int sum);
-}
