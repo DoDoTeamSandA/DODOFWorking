13f00f5f5c517c3b4eeb755172c5e041cb5d03d6
DoDoTeamSandA <dodsolutions4@gmail.com>
2021-05-20 11:49:45 +0530

    check 1

---
+++ DODO/Projects/DoDoF/app/src/main/java/com/sunny/dodof/Refund.java
@@ -0,0 +1,20 @@
+package com.sunny.dodof;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Refund extends AppCompatActivity {
+    private Toolbar refundToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refund);
+
+        refundToolbar = findViewById(R.id.refund_toolbar);
+        setSupportActionBar(refundToolbar);
+        getSupportActionBar().setTitle("Cancellation/Refund Policies:");
+    }
+}
---
+++ DODO/Projects/DoDoF/app/src/main/java/com/sunny/dodof/TotalListener.java
@@ -0,0 +1,6 @@
+package com.sunny.dodof;
+
+
+public interface TotalListener {
+    public void onTotalChanged(int sum);
+}
